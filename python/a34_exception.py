12199754d966da6db248db711a35f486e9fac431
leemc5612 <leemc5612@naver.com>
2024-09-20 15:50:22 +0900

    dd

--- python/a34_exception.py
+++ python/a34_exception.py
@@ -1,4 +1,28 @@
-def main():
+class Myerror(Exception):
+    def __init__(self, *args):
+        super().__init__(*args)
+        self.args = ['이것은 내가 만든 새롬 입니다']
 
+def check_neggtive_number(number):
+    if number < 0 :
+        raise Myerror
+
+def main():
+    li1 = [1,2,3,4,5,6]
+    try:
+        number = int(input("정수 입력>"))
+        check_neggtive_number(number)
+        print(li1[number])
+    except ValueError as e:
+        print(type(e), e)
+    except IndexError as e:
+        print(type(e), e)
+    except Myerror as e:
+        print(type(e), e)
+    # except Exception as e:
+        # print(type(e), e)
+    finally:
+        print("프로그램 종료")
+        
 if __name__ == "__main__":
     main()
---
+++ python/a35_function_test2.py
@@ -0,0 +1,14 @@
+import math
+
+import a34_exception
+
+a34_exception.Myerror
+
+def main():
+    math.tan()
+    math.sin
+
+if __name__ == "__main__":
+    main()
+
+
