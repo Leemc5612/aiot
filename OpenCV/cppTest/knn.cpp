8469324ac9b65b7393113653b6590e7327f3ba5f
leemc5612 <leemc5612@naver.com>
2024-10-17 09:22:40 +0900

    ss

--- OpenCV/cppTest/knn.cpp
+++ OpenCV/cppTest/knn.cpp
@@ -4,8 +4,8 @@
 using namespace cv;
 using namespace cv::ml;
 using namespace std;
-
 String folder = "/home/lmc/aiot/OpenCV/cppTest/data/";
+
 Ptr<KNearest> train_knn();
 
 int main()
@@ -16,25 +16,36 @@ int main()
     imshow("img", img);
 
     waitKey();
-
     return 0;
 }
 
-Ptr<KNearest> train_knn(){
-    Mat digits = imread(folder + "digits.png", IMREAD_GRAYCOLOR);
-    Mat train_images, train_labels;
+Ptr<KNearest> train_knn()
+{
+    Mat digits = imread(folder + "digits.png", IMREAD_GRAYSCALE);
 
-    for (int j = 0; j < 50; j++){
-        for (int i = 0; i<100; i++){
+    Mat train_images, train_labels;
+    int idx = 0;
+    for (int j = 0; j < 50; j++)
+    {
+        for (int i = 0; i < 100; i++)
+        {
             Mat roi, roi_float, roi_flatten;
             roi = digits(Rect(i * 20, j * 20, 20, 20));
+            cout << i << j << endl;
             roi.convertTo(roi_float, CV_32F);
-            roi_flatten = roi_float.reshape(-1, 1);
+            roi_flatten = roi_float.reshape(1, 1);
 
-            train_images.push_back(roi_flatten);
-            train_label.push_back(j/5);
+            // 평탄화된 이미지를 train_images에 복사
+            roi_flatten.copyTo(train_images.row(idx));
+
+            // 레이블 할당
+            train_labels.push_back(j / 5);
+
+            idx++;
         }
     }
 
-    Ptr<KNearest>
+    Ptr<KNearest> knn = KNearest::create();
+    knn->train(train_images, ROW_SAMPLE, train_labels);
+    return knn;
 }
