d2d4be9758eb8a894db13efea93fd78914eb7332
leemc5612 <leemc5612@naver.com>
2024-11-07 16:03:48 +0900

    dd

--- aiot_ws/src/simple_ros_cpp/src/pub_default.cpp
+++ aiot_ws/src/simple_ros_cpp/src/pub_default.cpp
@@ -1,3 +1,5 @@
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/string.hpp"
 #include <chrono>
 #include <iostream>
 
@@ -7,50 +9,32 @@ using namespace std::chrono_literals;
 class HellowPublisher : public rclcpp::Node
 {
 public:
-    HellowPublisher();
+    explicit HellowPublisher()
+        : Node("hello"), _count(0)
+    {
+        _pub = create_publisher<std_msgs::msg::String>("message", 10);
+        _timer = create_wall_timer(1s, std::bind(&HellowPublisher::printHello, this));
+    }
 
 private:
     int _count;
     rclcpp::Publisher<std_msgs::msg::String>::SharedPtr _pub;
     rclcpp::TimerBase::SharedPtr _timer;
-    void printHello();
+    void printHello()
+    {
+        auto msg = std_msgs::msg::String();
+        msg.data = "Hello, World!!!!! " + to_string(_count);
+        _pub->publish(msg);
+        RCLCPP_INFO(get_logger(), msg.data.c_str());
+        _count++;
+    }
 };
-
-#endif // __HELLO_PUB_CLASS_HPP__
-using namespace std;
-using namespace std::chrono_literals;
-void printHello();
 
 int main()
 {
     rclcpp::init(0, nullptr);
-    // node = Node("hello")
-    auto node = std::make_shared<rclcpp::Node>("hello");
-    auto timer = node ->create_wall_timer(1s, printHello);
+    auto node = std::make_shared<HellowPublisher>();
     rclcpp::spin(node);
     rclcpp::shutdown();
     return 0;
-
-
-}
-
-void printHello(){
-    static int count;
-    cout << "Hello, World!!!!!!" <<count<< endl;
-    count++;
-}
-HellowPublisher::HellowPublisher()
-    : Node("hello"), _count(0)
-{
-    _pub = create_publisher<std_msgs::msg::String>("message", 10);
-    _timer = create_wall_timer(1s, std::bind(&HellowPublisher::printHello, this));
-}
-
-void HellowPublisher::printHello()
-{
-    auto msg = std_msgs::msg::String();
-    msg.data = "Hello, World!!!!! " + to_string(_count);
-    _pub->publish(msg);
-    RCLCPP_INFO(get_logger(), msg.data.c_str());
-    _count++;
 }
